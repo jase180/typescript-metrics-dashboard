1cb5d5979c7092e9612dced4c45b37ed0208bad9
jason <jase180@gmail.com>
2025-10-05 20:03:38 -0500

    Initial playaround hello world typescript!

---
+++ type.ts
@@ -0,0 +1,12 @@
+let count: number = 42;
+let nameee: string = "Metrics Dashboard";
+let isActive: boolean = true;
+
+interface TradingMetrics {
+  pnl: number;
+  winRate: number;
+  totalTrades: number;
+  activePositions: number;
+}
+
+console.log("Hello World");
